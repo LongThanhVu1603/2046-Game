0c6f620d1a50ab590018625d4497fc986cd5e3aa
Vũ Thành Long <57677949+LongThanhVu1603@users.noreply.github.com>
2022-03-30 23:20:45 +0700

    30/3

--- js/grid.js
+++ js/grid.js
@@ -1,15 +1,27 @@
 
 //Set number of row and column in game
-const Grid_size = 4
+const Grid_size = 3
 const Cell_size = 20 
 const Cell_gap = 2
 
-export default class Grid {
+export default class Grid{
     constructor(gridElement){
         gridElement.style.setProperty("--grid-size", Grid_size)
-        gridElement.style.setProperty("--cell-size", `${Cell_size} vmin`)
-        gridElement.style.setProperty("--cell-gap", `${Cell_gap} vmin`)
+        gridElement.style.setProperty("--cell-size", `${Cell_size}vmin`)
+        gridElement.style.setProperty("--cell-gap", `${Cell_gap}vmin`)
         createCellElements(gridElement)
     }
 }
 
+//Then delete HTML we add cells by JS (change Grid_size)
+function createCellElements(gridElement) {
+    const cells = []
+
+    for (let i = 0; i < Grid_size * Grid_size; i++){
+        const cell = document.createElement('div')
+        cell.classList.add("cell")
+        cells.push(cell)
+        gridElement.append(cell)
+    }
+    return cells
+}
